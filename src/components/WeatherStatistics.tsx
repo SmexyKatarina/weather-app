2a33621acf8f2032fe2ee3ebea9e7fb8c4fa0fb1
Kat <mercier200012@hotmail.com>
2024-11-05 18:16:16 -0500

    - Rounded off the decimal values to make it look more readable - Changed fonts sizes to be bigger and easier to read. - Made more space for footer at the bottom so the disclaimer is not hidden

--- src/components/WeatherStatistics.tsx
+++ src/components/WeatherStatistics.tsx
@@ -19,13 +19,13 @@ const WeatherStatistics = (props: { weatherStats: { latitude: number, longitude:
             hourlyElements.push((
                 <div className="hour-tile">
                     <div className="hour-time">{time.split("T").join(" ")}</div>
-                    <div className="hour-temp">Temp: {temp}°C (Feels Like: {apparent}°C)</div>
-                    <div className="hour-wind">Wind Speed: {wind}Km/h</div>
+                    <div className="hour-temp">Temp: {Math.round(temp)}°C<br/>(Feels Like: {Math.round(apparent)}°C)</div>
+                    <div className="hour-wind">Wind Speed: {Math.round(wind)}Km/h</div>
                 </div>
             ))
             
         }
-        return hourlyElements
+        return hourlyElements;
     }
 
     return (
@@ -33,8 +33,9 @@ const WeatherStatistics = (props: { weatherStats: { latitude: number, longitude:
             <div className="location">Location: {latitude}°N {longitude}°E with an elevation of {elevation}m {(elevation < 0) ? "below" : "above"}</div>
             <div className="timezone">Time: {currentWeather.time.split("T").join(" ") + " " + timezone_abbreviation} ({timezone})</div>
             <div className="current-weather">
-                <div className="current-temp">Temp: {currentWeather.temperature_2m}°C (Feels like: {currentWeather.apparent_temperature}°C)</div>
-                <div className="current-wind">Wind Speed: {currentWeather.wind_speed_10m}Km/h</div>
+                <div>Current Weather</div>
+                <div className="current-temp">Temp: {Math.round(currentWeather.temperature_2m)}°C (Feels like: {Math.round(currentWeather.apparent_temperature)}°C)</div>
+                <div className="current-wind">Wind Speed: {Math.round(currentWeather.wind_speed_10m)}Km/h</div>
             </div>
             <div className="hourly-weather">
                 {generateHourly().map(x => { return x; })}
