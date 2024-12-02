a8139fe591a8d19a19186baa046080b7080fc840
Kat <mercier200012@hotmail.com>
2024-12-02 00:31:34 -0500

    Added link to icon website

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,6 +8,7 @@ const Footer = () => {
         <div id="footer">
             <a href="https://www.github.com/smexykatarina/" rel="noreferrer" target="_blank" className="footer-link">Weather app made by Titus</a>
             <a href="https://smexykatarina.github.io/" rel="noreferrer" target="_blank" className="footer-link">My Portfolio</a>
+            <a href="https://www.flaticon.com/authors/freepik" title="cloud icons" target="_blank" rel="noreferrer" className="footer-link">Icons</a>
             <a href="https://www.github.com/smexykatarina/weather-app" rel="noreferrer" target="_blank" className="footer-link">See the code here...</a>
         </div>
     );
